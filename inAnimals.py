ff6292b4357701e8c7a28251b840c44950d59ae0
Gregory <evgenii_kip@mail.ru>
2022-11-21 21:35:23 +0300

    сохранение с ошибками

--- inAnimals.py
+++ inAnimals.py
@@ -1,13 +1,77 @@
 class Lion:
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, food):
         self.Name=name
         self.Age=age
-        self.IndexPushistosti=100
+        self.vid="Лев"
+        self.Biom="Савана"
+        self.ploshad="> 20 м кв"
+        self.pisha="рыба, мясо"
+        self.eda="хищник"
+        self.zvyk="Рёв"
+        #животное
+        self.Name = name
+        self.Age = age
+        self.Food = food
 
-    def DoRfav(self, number):
+    def Voice(self, number):
         for i in range(number):
-            print(self.Name, "Rfaaavv")
+            print(self.Name, "РААААВВВ")
 
-    def Griva(self):
-        self.IndexDlinigrivi-=10
+    def Eat(self):
+        return self.name, "Чавк Ном"
+
+    def Play(self):
+        return self.name, "прыг-скок"
+
+class Panda:
+
+    def __init__(self, name, age, food):
+        self.Name=name
+        self.Age=age
+        self.vid="Панда"
+        self.Biom="Бамбуковые леса"
+        self.ploshad="> 30 м кв"
+        self.pisha="бамбуковые"
+        self.eda="травоядные"
+        self.zvyk="игигиги(звуки заводящегося жигуля( не знаю как это назвать по-другому))"
+        #животное
+        self.Name = name
+        self.Age = age
+        self.Food = food
+
+    def Voice(self, number):
+        for i in range(number):
+            print(self.Name, "иггигиги")
+
+    def Eat(self):
+        return self.name, "хрум-хрум-хрум глык"
+
+    def Play(self):
+        return self.name, "*перекатывается из стороны в сторону"
+
+class Fox:
+
+    def __init__(self, name, age, food):
+        self.Name=name
+        self.Age=age
+        self.vid="Лиса"
+        self.Biom="Тундра"
+        self.ploshad="> 25 м кв"
+        self.pisha="мясо"
+        self.eda="курица, грызу"
+        self.zvyk="фыр-фыр"
+        #животное
+        self.Name = name
+        self.Age = age
+        self.Food = food
+
+    def Voice(self, number):
+        for i in range(number):
+            print(self.Name, "фыр-фыр-фыр")
+
+    def Eat(self):
+        return self.name, "чавк чвак"
+
+    def Play(self):
+        return self.name, "прыгает и бугает в разные стороны"
--- zoopark.py
+++ zoopark.py
@@ -1,13 +1,15 @@
-from Lion import *
-l1=Lion("Mikhail Krug", 12)
-l2=Lion("Metab", 6)
+from inAnimals import *
+l1=Lion("Михаил Круг", 12, 30)
+l1.Voice(2)
+l1.Eat(print)
+l1.Play(print)
 
-Lions=[]
-Lions.append(c1)
-Lions.append(Cat("Qwe", 9))
-Lions.append(c2)
+P1=Panda("Гречка", 12, 30)
+P1.Voice(2)
+P1.Eat()
+P1.Play()
 
-c1.Strizca()
-
-print(c1.IndexPushistosti)
-print(c2.IndexPushistosti)
+f1=Fox("Сквозь Леса", 12, 30)
+f1.Voice(2)
+f1.Eat()
+f1.Play()
